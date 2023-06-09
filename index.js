78b450cc70394a606fee6687b0aab2315efe9388
v-house <134326034+v-house@users.noreply.github.com>
2023-06-09 10:49:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ app.use(cookies());
 app.use(express.json());
 
 const client = new Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  user: process.env.DB1_USER,
+  host: process.env.DB1_HOST,
+  database: process.env.DB1_DATABASE,
+  password: process.env.DB1_PASSWORD,
   port: process.env.DB_PORT,
 });
 
